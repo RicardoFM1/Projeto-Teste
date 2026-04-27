acd91f03ada458a0873f85ee8a670a8cbf32dddd
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2026-04-27 18:25:08 -0300

    feat(adicionado cards e tabelas)

--- Frontend/src/components/Dashboard/dashboard.jsx
+++ Frontend/src/components/Dashboard/dashboard.jsx
@@ -1,8 +1,13 @@
-
+import DashboardCard from "../Cards/Dashboard/dashboardCard"
+import DataTable from "../Table/datatable"
 
 function Dashboard () {
     return (
+        <>
         <h1>Dashboard</h1>
+        <DashboardCard/>
+        <DataTable/>
+        </>
     )
 }
 
---
+++ Frontend/src/components/Table/datatable.jsx
@@ -0,0 +1,44 @@
+import Table from "react-bootstrap/Table"
+
+function DataTable () {
+    return (
+    <Table bordered hover >
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Nome</th>
+                <th>Nome</th>
+                <th>Nome</th>
+                <th>Nome</th>
+        
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Algo</td>
+                <td>Algo</td>
+                <td>Algo</td>
+                <td>Algo</td>
+                <td>Algo</td>
+              
+
+            </tr>
+             <tr>
+                <td>Algo</td>
+                <td>Algo</td>
+                <td>Algo</td>
+                <td>Algo</td>
+                <td>Algo</td>
+              
+
+            </tr>
+        </tbody>
+    </Table>
+    )
+
+}
+
+// Aprender a mapear dados e colocar na tabela.
+// Customizar a tabela
+
+export default DataTable
