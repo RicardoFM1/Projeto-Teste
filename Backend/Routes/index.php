a3ee094cd1d36526ce60852810742d8f581b1477
RicardoFM1 <ricardofernandesdemoura7@gmail.com>
2026-04-29 16:01:36 -0300

    Adicionado dashboard no backend

--- Backend/Routes/index.php
+++ Backend/Routes/index.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/../Controllers/Mesa/mesaController.php';
 require_once __DIR__ . '/../Controllers/Convidado/convidadoController.php';
 require_once __DIR__ . '/../Controllers/Checkin/checkinController.php';
 require_once __DIR__ . '/../Controllers/Acompanhante/acompanhanteController.php';
+require_once __DIR__ . "/../Controllers/Dashboard/dashboardController.php";
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE');
@@ -125,5 +126,15 @@ if ($caminhoRequisicao === '/acompanhante') {
     }
     if ($metodoRequisicao === 'DELETE') {
         $acompanhanteController->deletarAcompanhante();
+    }
+}
+
+// Rota Dashboard: 
+
+if ($caminhoRequisicao === '/dashboard') {
+    $dashboardController = new DashboardController();
+
+    if ($metodoRequisicao === 'GET') {
+        $dashboardController->listarDashboard();
     }
 }
